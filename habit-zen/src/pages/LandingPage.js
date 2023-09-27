249a4ab6a2e142c49130537602e29ff2439d3e43
Hawariaw Paulos <108014859+Hawa701@users.noreply.github.com>
2023-09-28 00:27:24 +0300

    Update LandingPage.js

--- habit-zen/src/pages/LandingPage.js
+++ habit-zen/src/pages/LandingPage.js
@@ -76,8 +76,13 @@ const LandingPage = () => {
         </Box>
       </Box>
 
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <Typography variant="h6" fontFamily="Montserrat" fontWeight="bold">
+      <Box display="flex" flexDirection="column">
+        <Typography
+          variant="h6"
+          textAlign="center"
+          fontFamily="Montserrat"
+          fontWeight="bold"
+        >
           Famous Quotes
         </Typography>
         <AliceCarousel autoPlay autoPlayInterval={7000}>
